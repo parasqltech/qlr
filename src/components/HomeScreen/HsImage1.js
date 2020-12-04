fb87653da96007efc1784c1f8564060c3f73fe63
Paras <paras@qltech.com.au>
2020-12-04 13:05:17 +0530

    changes for link home

--- src/components/HomeScreen/HsImage1.js
+++ src/components/HomeScreen/HsImage1.js
@@ -41,7 +41,7 @@ export default () => (
                         <Card.Body className="pl-0 pr-0 pb-0 h-100">
                           <Card.Title className="font-22 font-bold">{prop.node.acf.title1}</Card.Title>
                           <Card.Text className=" font-14 font-regular">{prop.node.acf.title2}</Card.Text>
-                          <Link to={prop.node.acf.link} className="nav-link p-0 font-18 font-regular">Learn more<FaAngleRight className="pl-2" /> </Link>
+                          <Link to={"/"+prop.url.toLowerCase().replace("http://", '')} className="nav-link p-0 font-18 font-regular">Learn more<FaAngleRight className="pl-2" /> </Link>
                         </Card.Body>
                       </Card>
                     </Col>
