5348f6c2a81dfed379d48e4acb0e1c44fd59fbe3
Paras <paras@qltech.com.au>
2020-12-11 17:23:36 +0530

    commit 11-12-2020 5:23PM

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -80,7 +80,7 @@ class Footer extends Component {
                                      <Image src={footermail} className="img-fluid "/>
                                   </div>    
                                   <div className="footer-arrow-data last">
-                                    <a href="mailto:raj@qlresources.com.au" className="nav-link font-16 font-regular">raj@qlresources.com.au</a>
+                                    <a href="mailto:support@qlresoureces.com.au" className="nav-link font-16 font-regular">support@qlresoureces.com.au</a>
                                   </div>
                               </div>
                             </ListGroup.Item>
