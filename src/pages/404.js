a2eec572c337e0d9617a3ed779a8a4158f92820b
Paras <paras@qltech.com.au>
2020-12-15 14:12:26 +0530

    Remove unnecessary file

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Container } from 'react-bootstrap';
 
 const NotFoundPage = () => (
   <Layout>
     <div>
+    <Container>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
     </div>
   </Layout>
 )
