c5b5df38f3c7f3dae232e395cf9a42294e349b7a
parasqltech <33412121+parasqltech@users.noreply.github.com>
2022-02-14 10:51:27 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,40 +7,32 @@ export default class IndexPage extends React.Component {
     const { posts, title } = this.props
 
     return (
-      <section className="section">
+      <section className="section home-section-3">
         <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">{title}</h1>
-          </div>
+         
           {posts.map(({ node: post }) => (
-            <div
-              className="content"
-              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-              key={post.id}
-            >
-              <p>
-                <Link className="has-text-primary" to={post.slug}>
-                  {post.title}
-                </Link>
-                <span> &bull; </span>
-                <small>
+		  
+		  
+			<div className="md-mb-4 h-100 col-xl-4 col-lg-4 col-md-6 col-sm-12">
+				<div className="p-0 border-0 card">
+					<img className="card-img-top" src="https://admin.qlresources.com.au/wp-content/uploads/2020/11/Home-Mining-1.jpg"/>
+					<div className="pl-0 pr-0 pb-0 h-100 card-body">
+						<div className="font-22 font-bold card-title h5">{post.title}</div>
+							<p><small>
                   {post.date} - posted by{' '}
                   <Link to={`/author/${post.author.slug}`}>
                     {post.author.name}
                   </Link>
-                </small>
-              </p>
-              <div>
-                <div
-                  dangerouslySetInnerHTML={{
+                </small></p>
+							<p className=" font-16 font-regular card-text" dangerouslySetInnerHTML={{
                     __html: post.excerpt.replace(/<p class="link-more.*/, ''),
-                  }}
-                />
-                <Link className="button is-small" to={post.slug}>
-                  Keep Reading →
-                </Link>
-              </div>
-            </div>
+                  }} />
+								<Link className="nav-link p-0 font-18 font-regular" to={post.slug} >Keep Reading →
+									<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" class="pl-2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg> 
+								</Link>
+					</div>
+				</div>
+			</div>
           ))}
         </div>
       </section>
