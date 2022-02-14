d353d13d3f79356e74c6fff2b949e07270dbf3e6
parasqltech <33412121+parasqltech@users.noreply.github.com>
2022-02-14 11:15:11 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -24,9 +24,9 @@ export default class IndexPage extends React.Component {
                     {post.author.name}
                   </Link>
                 </small></p>
-							<p className=" font-16 font-regular card-text" dangerouslySetInnerHTML={{
-                    __html: post.excerpt.replace(/<p class="link-more.*/, ''),
-                  }} />
+							<p className=" font-16 font-regular card-text" dangerouslySetInnerHTML={{ __html: post.excerpt.substring(0, 500)+"...."}} />
+		  
+		  
 								<Link className="nav-link p-0 font-18 font-regular" to={post.slug} >Keep Reading →
 									<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 512" class="pl-2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg> 
 								</Link>
