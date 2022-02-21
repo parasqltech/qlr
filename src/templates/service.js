ae188322d8000669fd7c34c9c5334e4ba8d7b226
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-02-21 12:38:55 +0530

    Update service.js

--- src/templates/service.js
+++ src/templates/service.js
@@ -14,7 +14,7 @@ super(props);
 componentDidMount() {
    if(window.location.hash) {
  	var hash = window.location.hash.substring(1);
-	$('html,body').unbind().animate({scrollTop: $(hash).offset().top-50},'slow');
+	$('html,body').unbind().animate({scrollTop: $("#"+hash).offset().top-50},'slow');
   }
 }
 
