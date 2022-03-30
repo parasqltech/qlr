83432be23acad46f649b839b7675936e4c4f16cc
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2022-03-30 10:26:12 +0530

    Update ContactScreen2.js

--- src/components/ContactScreen/ContactScreen2.js
+++ src/components/ContactScreen/ContactScreen2.js
@@ -54,14 +54,14 @@ export default () => (
 				  
   <Row>
     <Col>
-				<h3>Australia Office (Headquarters)</h3><Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3385.1889096226905!2d115.859075!3d-31.9557714!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32bb29e26a22b5%3A0x4237fca06c578d13!2s45%20St%20Georges%20Terrace%2C%20Perth%20WA%206000%2C%20Australia!5e0!3m2!1sen!2sin!4v1598633772764!5m2!1sen!2sin"
+				<h3 className="font-25 font-regular text-center mb-30">Australia Office (Headquarters)</h3><Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3385.1889096226905!2d115.859075!3d-31.9557714!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32bb29e26a22b5%3A0x4237fca06c578d13!2s45%20St%20Georges%20Terrace%2C%20Perth%20WA%206000%2C%20Australia!5e0!3m2!1sen!2sin!4v1598633772764!5m2!1sen!2sin"
                         width="100%"
                         height="500px"
                         className="map-iframe"
                         position="relative"
                         frameBorder="0" 
                         allowFullScreen/></Col>
-    <Col><h3>India Office</h3><iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.9942814275378!2d72.5655622145081!3d23.023982184952448!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84fcf0805947%3A0x5a6d3c9ec65ad9b8!2sQL%20Tech%20%C2%BB%20Web%20Design%20%26%20SEO%20Marketing%20Company!5e0!3m2!1sen!2sin!4v1570855705668!5m2!1sen!2sin" width="100%"
+    <Col><h3 className="font-25 font-regular text-center mb-30">India Office</h3><iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.9942814275378!2d72.5655622145081!3d23.023982184952448!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84fcf0805947%3A0x5a6d3c9ec65ad9b8!2sQL%20Tech%20%C2%BB%20Web%20Design%20%26%20SEO%20Marketing%20Company!5e0!3m2!1sen!2sin!4v1570855705668!5m2!1sen!2sin" width="100%"
                         height="500px"
                         className="map-iframe"
                         position="relative"
