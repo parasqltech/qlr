b416ae42a5a13faf9be8abaf643bfb9667931c00
parasqltech <33412121+parasqltech@users.noreply.github.com>
2023-02-22 12:25:28 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -17,7 +17,7 @@ console.log("here");
 		  
 			<div className="md-mb-4 h-100 col-xl-4 col-lg-4 col-md-6 col-sm-12">
 				<div className="p-0 border-0 card">
-					
+					{(post.acf.image != null) ? (<><img className="card-img-top" src={post.acf.image.source_url}/></>) : ("") }
 					<div className="pl-0 pr-0 pb-0 h-100 card-body">
 						<div className="font-22 font-bold card-title h5" dangerouslySetInnerHTML={{ __html: post.title }} />
 							<p><small>
