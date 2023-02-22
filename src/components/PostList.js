8cab490259a9ada640a33a003947aa951c659b17
parasqltech <33412121+parasqltech@users.noreply.github.com>
2023-02-22 11:08:00 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -15,7 +15,7 @@ export default class IndexPage extends React.Component {
 		  
 			<div className="md-mb-4 h-100 col-xl-4 col-lg-4 col-md-6 col-sm-12">
 				<div className="p-0 border-0 card">
-					<img className="card-img-top" src={post.acf.image}/>
+					<img className="card-img-top" src={post.acf.image.source_url}/>
 					<div className="pl-0 pr-0 pb-0 h-100 card-body">
 						<div className="font-22 font-bold card-title h5">{post.title}</div>
 							<p><small>
@@ -50,7 +50,9 @@ export const pageQuery = graphql`
    
 	acf {
           
-          image 
+          image {
+	  	source_url
+	  }
         }  
     date(formatString: "MMMM DD, YYYY")
     slug
