a796c0ec988efdf6aa744491d08ca141370fcbd3
parasqltech <33412121+parasqltech@users.noreply.github.com>
2023-07-31 14:55:38 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,7 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="QL-Resources" link={[{ rel: "shortcut icon", type: "image/png", href: `${favicon}` }]} />
       <Helmet>
+  <meta name="google-site-verification" content="jKISjo3LeS2Bf8cf2XHd0BXy6muyMu6dj7vKpm1IIFU" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
         
